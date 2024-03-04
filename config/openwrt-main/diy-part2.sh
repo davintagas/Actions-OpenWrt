2230cf2bb4461620fbad6b4c907ada1c9809901b
davintagas <78451092+davintagas@users.noreply.github.com>
2024-03-04 19:49:44 +0700

    Update diy-part2.sh

--- config/openwrt-main/diy-part2.sh
+++ config/openwrt-main/diy-part2.sh
@@ -12,6 +12,8 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Add the default password for the 'root' user（Change the empty password to 'password'）
+sed -i 's/root:::0:99999:7:::/root:$1$wGIpIvlO$H3XgR7517Ex7I6fQ2jHwv0:19786:0:99999:7:::/g' package/base-files/files/etc/shadow
 # Add Modeminfo
 git clone --depth 1 https://github.com/4IceG/luci-app-3ginfo-lite.git 3ginfo
 mv 3ginfo/luci-app-3ginfo-lite package/
