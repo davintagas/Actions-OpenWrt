519bf92a131ca584e65b145253301207e0bd2a4b
davintagas <78451092+davintagas@users.noreply.github.com>
2024-03-05 09:41:29 +0700

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -12,6 +12,58 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Add the default password for the 'root' user（Change the empty password to 'password'）
+sed -i 's/root::0:0:99999:7:::/root:$1$wGIpIvlO$H3XgR7517Ex7I6fQ2jHwv0:19786:0:99999:7:::/g' package/base-files/files/etc/shadow
+# Add Modeminfo
+git clone --depth 1 https://github.com/4IceG/luci-app-3ginfo-lite.git 3ginfo
+mv 3ginfo/luci-app-3ginfo-lite package/
+rm -rf 3ginfo
+# Add sms
+git clone --depth 1 https://github.com/4IceG/luci-app-sms-tool-js.git app-sms
+mv app-sms/luci-app-sms-tool-js package/
+rm -rf app-sms
+# Add internet-detector
+git clone --depth 1 https://github.com/gSpotx2f/luci-app-internet-detector.git internet-detector
+mv internet-detector/{luci-app-internet-detector,internet-detector,internet-detector-mod-modem-restart} package/
+rm -rf internet-detector
+# Add theme
+git clone -b js --depth 1 https://github.com/gngpp/luci-theme-design.git package/luci-theme-design
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone --depth 1 https://github.com/derisamedia/luci-theme-alpha.git package/luci-theme-alpha
+
+# Change base
+# git clone --depth 1 https://github.com/kiddin9/openwrt-packages.git kiddin
+# rm -rf feeds/luci/modules/{luci-base,luci-mod-status}
+# cp -r kiddin/{luci-base,luci-mod-status} feeds/luci/modules/
+# Add autocore
+# cp -r kiddin/autocore package/
+# sed -i 's/utils/userland/g' package/autocore/Makefile
+# Add diskman
+cp -r kiddin/luci-app-diskman package/luci-app-diskman
+# Add ramfree
+cp -r kiddin/luci-app-ramfree package/luci-app-ramfree
+# Add fullconenat
+rm -rf package/network/config/firewall
+cp -r kiddin/firewall package/network/config/
+cp -r kiddin/fullconenat package/fullconenat
+# Add turboacc
+cp -r kiddin/{shortcut-fe,dnsproxy,dnsforwarder,pdnsd-alt,luci-app-turboacc} package/
+# Delete
+rm -rf kiddin
+# Add mosdns
+git clone --depth 1 https://github.com/sbwml/luci-app-mosdns.git mosdns
+mv mosdns/{luci-app-mosdns,mosdns,v2dat} package/
+rm -rf mosdns
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/passwall_packages/v2ray-geodata
+git clone --depth 1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+# Add Default Setting
+mkdir -p files/etc/uci-defaults
+pushd files/etc/uci-defaults
+wget https://raw.githubusercontent.com/davintagas/default/main/official/99-init-settings.sh
+popd
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
