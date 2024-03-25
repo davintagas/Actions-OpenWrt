921b85379f5dc77d1604e0558698aaeec957f2a3
davintagas <78451092+davintagas@users.noreply.github.com>
2024-03-25 12:49:59 +0700

    Create diy-part2.sh

---
+++ config/ROOter/diy-part2.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Add the default password for the 'root' user（Change the empty password to 'password'）
+sed -i 's/root:::0:99999:7:::/root:$1$wGIpIvlO$H3XgR7517Ex7I6fQ2jHwv0:19786:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+# Add internet-detector
+git clone --depth 1 https://github.com/gSpotx2f/luci-app-internet-detector.git internet-detector
+mv internet-detector/{luci-app-internet-detector,internet-detector,internet-detector-mod-modem-restart} package/
+rm -rf internet-detector
+
+# Add theme
+git clone -b js --depth 1 https://github.com/gngpp/luci-theme-design.git package/luci-theme-design
+git clone --depth 1 https://github.com/derisamedia/luci-theme-alpha.git package/luci-theme-alpha
+
+# Add mosdns
+git clone --depth 1 https://github.com/sbwml/luci-app-mosdns.git mosdns
+mv mosdns/{luci-app-mosdns,mosdns,v2dat} package/
+rm -rf mosdns
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/net/mosdns
+git clone --depth 1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# Add app
+git clone --depth 1 https://github.com/kiddin9/openwrt-packages.git kiddin
+# Add ramfree
+cp -r kiddin/luci-app-ramfree package/luci-app-ramfree
+# Add cpufreq
+cp -r kiddin/{luci-app-cpufreq,cpufreq} package/
+# Delete
+rm -rf kiddin
+
+# Add Default Setting
+mkdir -p files/etc/uci-defaults
+pushd files/etc/uci-defaults
+wget https://raw.githubusercontent.com/davintagas/default/main/official/99-init-settings.sh
+popd
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
