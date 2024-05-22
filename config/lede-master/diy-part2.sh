e3fcdf6d4b666c742b151f7aeaf5fe419f29c0c8
davintagas <78451092+davintagas@users.noreply.github.com>
2024-05-22 08:54:20 +0700

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -9,8 +9,9 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# ./scripts/feeds update -a && ./scripts/feeds install -a -f
-rm -rf feeds/luci/applications/luci-app-v2raya
+echo 'src-git kiddin https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a -f
+# rm -rf feeds/luci/applications/luci-app-v2raya
 # rm -rf package/lean/default-settings
 
 # Modify default IP
@@ -23,9 +24,9 @@ git clone --depth 1 https://github.com/4IceG/luci-app-3ginfo-lite.git package/lu
 # rm -rf package/luci-app-3ginfo-lite/sms-tool
 
 # Add internet-detector
-git clone --depth 1 https://github.com/gSpotx2f/luci-app-internet-detector.git internet-detector
-mv internet-detector/{luci-app-internet-detector,internet-detector,internet-detector-mod-modem-restart} package/
-rm -rf internet-detector
+# git clone --depth 1 https://github.com/gSpotx2f/luci-app-internet-detector.git internet-detector
+# mv internet-detector/{luci-app-internet-detector,internet-detector,internet-detector-mod-modem-restart} package/
+# rm -rf internet-detector
 
 # Add theme
 #rm -rf feeds/luci/themes/luci-theme-argon
@@ -34,32 +35,32 @@ rm -rf internet-detector
 #git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 # Add source
-git clone --depth 1 https://github.com/kiddin9/openwrt-packages.git kiddin
+# git clone --depth 1 https://github.com/kiddin9/openwrt-packages.git kiddin
 
-rm -rf feeds/packages/net/mosdns
-cp -r kiddin/mosdns feeds/packages/net/mosdns
-cp -r kiddin/luci-app-mosdns package/luci-app-mosdns
+# rm -rf feeds/packages/net/mosdns
+# cp -r kiddin/mosdns feeds/packages/net/mosdns
+# cp -r kiddin/luci-app-mosdns package/luci-app-mosdns
 
 # Add sms-tool
-cp -r kiddin/luci-app-sms-tool-js package/luci-app-sms-tool-js
+# cp -r kiddin/luci-app-sms-tool-js package/luci-app-sms-tool-js
 
 # Add modemband
-cp -r kiddin/{luci-app-modemband,modemband} package/
+# cp -r kiddin/{luci-app-modemband,modemband} package/
 
 # Add atinout
-cp -r kiddin/{luci-app-atinout,atinout} package/
+# cp -r kiddin/{luci-app-atinout,atinout} package/
 
 # Add driver L860-GL
-cp -r kiddin/{luci-proto-xmm,xmm-modem} package/
+# cp -r kiddin/{luci-proto-xmm,xmm-modem} package/
 
 # Add ramfree
-cp -r kiddin/luci-app-ramfree package/luci-app-ramfree
+# cp -r kiddin/luci-app-ramfree package/luci-app-ramfree
 
 # Add turboacc
-cp -r kiddin/luci-app-turboacc package/luci-app-turboacc
+# cp -r kiddin/luci-app-turboacc package/luci-app-turboacc
 
 # Delete
-rm -rf kiddin
+# rm -rf kiddin
 
 # Add mosdns
 # rm -rf feeds/packages/utils/v2dat
@@ -83,3 +84,4 @@ sed -i 's|CST-8|WIB-7|g' package/lean/default-settings/files/zzz-default-setting
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+rm -rf feeds/kiddin/{ntfs3-mount,nftables,shortcut-fe,fibocom-dial,libiptext,libiptext6,fullconenat,arptables-nft,ebtables-nft,xtables-nft,adbyby,dnsmasq,minisign,quectel_cm_5G,quectel_Gobinet,luci-app-gobinetmodem,firewall4,quectel_MHI,opkg,luci-app-pcimodem,ppp,ps3netsrv,quectel_SRPD_PCIE,luci-app-spdmodem,swanmon,vsftpd-alt,fast-classifier,simulated-driver,fibocom_QMI_WWAN,quectel_QMI_WWAN,luci-app-usbmodem,libnftnl,shellsync,ucl,firewall,luci-app-dnscrypt-proxy2,luci-app-strongswan-swanctl,my-default-settings}
