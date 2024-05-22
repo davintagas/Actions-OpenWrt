df5961d888d66fbe1fd6e4e776a515a71c4e027c
davintagas <78451092+davintagas@users.noreply.github.com>
2024-05-22 13:17:05 +0700

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -20,7 +20,7 @@
 sed -i 's/root:::0:99999:7:::/root:$1$wGIpIvlO$H3XgR7517Ex7I6fQ2jHwv0:19786:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Add modeminfo
-git clone --depth 1 https://github.com/4IceG/luci-app-3ginfo-lite.git package/luci-app-3ginfo-lite
+# git clone --depth 1 https://github.com/4IceG/luci-app-3ginfo-lite.git package/luci-app-3ginfo-lite
 # rm -rf package/luci-app-3ginfo-lite/sms-tool
 
 # Add internet-detector
