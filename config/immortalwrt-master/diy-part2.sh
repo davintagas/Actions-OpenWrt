5c7a02572633d619e302cb4cd52be926dc421bb0
davintagas <78451092+davintagas@users.noreply.github.com>
2024-05-28 09:15:27 +0700

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -15,37 +15,46 @@
 # Add the default password for the 'root' user（Change the empty password to 'password'）
 sed -i 's/root:::0:99999:7:::/root:$1$wGIpIvlO$H3XgR7517Ex7I6fQ2jHwv0:19786:0:99999:7:::/g' package/base-files/files/etc/shadow
 
-# Add internet-detector
-git clone --depth 1 https://github.com/gSpotx2f/luci-app-internet-detector.git internet-detector
-mv internet-detector/{luci-app-internet-detector,internet-detector,internet-detector-mod-modem-restart} package/
-rm -rf internet-detector
+# Add 3ginfo-lite
+git clone -b main --depth 1 https://github.com/4IceG/luci-app-3ginfo-lite.git package/3ginfo-lite
+rm -rf package/3ginfo-lite/{.github,sms-tool}
 
 # Add theme
 git clone -b js --depth 1 https://github.com/gngpp/luci-theme-design.git package/luci-theme-design
 git clone --depth 1 https://github.com/derisamedia/luci-theme-alpha.git package/luci-theme-alpha
 
-# Add mosdns
-git clone --depth 1 https://github.com/sbwml/luci-app-mosdns.git mosdns
-mv mosdns/{luci-app-mosdns,mosdns,v2dat} package/
-rm -rf mosdns
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
-rm -rf feeds/packages/net/v2ray-geodata
-rm -rf feeds/packages/net/mosdns
-git clone --depth 1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
-# Add Custom Package
+# Add Package From Kiddin
 git clone --depth 1 https://github.com/kiddin9/openwrt-packages.git kiddin
 # Add driver l860-GL
 cp -r kiddin/{luci-proto-xmm,xmm-modem} package/
 # Add Atinout
 cp -r kiddin/{luci-app-atinout,atinout} package/
 # Add sms-tools
-cp -r kiddin/luci-app-sms-tool package/luci-app-sms-tool
+cp -r kiddin/{luci-app-sms-tool-js,sms-tool} package/
+rm -rf feeds/packages/utils/sms-tool
 # Add modemband
 # cp -r kiddin/{luci-app-modemband,modemband} package/
+# Add internet-detector
+cp -r kiddin/luci-app-internet-detector package/luci-app-internet-detector
+# Add luci-app-modemband
+cp -r kiddin/{luci-app-modemband,modemband} package/
+# Change smartdns
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/applications/luci-app-smartdns
+cp -r kiddin/{luci-app-smartdns,smartdns} package/
 
 rm -rf kiddin
+
+# Change ModemManager
+git clone -b openwrt-23.05 --depth 1 https://github.com/openwrt/luci.git 23.05-luci
+rm -rf feeds/luci/protocols/luci-proto-modemmanager
+cp -r 23.05-luci/protocols/luci-proto-modemmanager feeds/luci/protocols/
+rm -rf 23.05-luci
+git clone -b openwrt-23.05 --depth 1 https://github.com/openwrt/packages.git 23.05-packages
+rm -rf feeds/packages/net/modemmanager
+cp -rf 23.05-packages/net/modemmanager feeds/packages/net/
+rm -rf 23.05-packages
+sed -i 's|include $(INCLUDE_DIR)/meson.mk||g' feeds/packages/net/modemmanager/Makefile
 
 # Add Default Setting
 sed -i 's/auto/en/g' package/emortal/default-settings/files/99-default-settings
