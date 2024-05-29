d7229a77f5ac9e5818ecda295143c3623f96815d
davintagas <78451092+davintagas@users.noreply.github.com>
2024-05-29 07:47:11 +0700

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -15,10 +15,6 @@
 # Add the default password for the 'root' user（Change the empty password to 'password'）
 sed -i 's/root:::0:99999:7:::/root:$1$wGIpIvlO$H3XgR7517Ex7I6fQ2jHwv0:19786:0:99999:7:::/g' package/base-files/files/etc/shadow
 
-# Add 3ginfo-lite
-git clone -b main --depth 1 https://github.com/4IceG/luci-app-3ginfo-lite.git package/3ginfo-lite
-rm -rf package/3ginfo-lite/{.github,sms-tool}
-
 # Add theme
 git clone -b js --depth 1 https://github.com/gngpp/luci-theme-design.git package/luci-theme-design
 git clone --depth 1 https://github.com/derisamedia/luci-theme-alpha.git package/luci-theme-alpha
@@ -29,19 +25,8 @@ git clone --depth 1 https://github.com/kiddin9/openwrt-packages.git kiddin
 cp -r kiddin/{luci-proto-xmm,xmm-modem} package/
 # Add Atinout
 cp -r kiddin/{luci-app-atinout,atinout} package/
-# Add sms-tools
-cp -r kiddin/{luci-app-sms-tool-js,sms-tool} package/
-rm -rf feeds/packages/utils/sms-tool
-# Add modemband
-# cp -r kiddin/{luci-app-modemband,modemband} package/
 # Add internet-detector
 cp -r kiddin/luci-app-internet-detector package/luci-app-internet-detector
-# Add luci-app-modemband
-cp -r kiddin/{luci-app-modemband,modemband} package/
-# Change smartdns
-rm -rf feeds/packages/net/smartdns
-rm -rf feeds/luci/applications/luci-app-smartdns
-cp -r kiddin/{luci-app-smartdns,smartdns} package/
 
 rm -rf kiddin
 
