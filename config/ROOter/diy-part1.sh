99faf6fcd979ad61be92075067706ea5b787c944
davintagas <78451092+davintagas@users.noreply.github.com>
2024-06-10 18:57:43 +0700

    Update diy-part1.sh

--- config/ROOter/diy-part1.sh
+++ config/ROOter/diy-part1.sh
@@ -15,17 +15,17 @@
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-git clone -b openwrt-23.05 --depth 1 https://github.com/immortalwrt/immortalwrt.git
-rm -rf target/linux/rockchip
-cp -r immortalwrt/target/linux/rockchip target/linux/
-rm -rf include/kernel-5.15
-cp -r immortalwrt/include/kernel-5.15 include/
-rm -rf package/boot/{arm-trusted-firmware-rockchip,uboot-rockchip}
-cp -r immortalwrt/package/boot/{arm-trusted-firmware-rockchip,uboot-rockchip} package/boot/
-cp -r target/linux/generic/pending-5.15/{900-driver2305.patch,900-option.patch,900-qcserial.patch} immortalwrt/target/linux/generic/pending-5.15
-rm -rf target/linux/generic
-cp -r immortalwrt/target/linux/generic target/linux/
-rm -rf immortalwrt
+#git clone -b openwrt-23.05 --depth 1 https://github.com/immortalwrt/immortalwrt.git
+#rm -rf target/linux/rockchip
+#cp -r immortalwrt/target/linux/rockchip target/linux/
+#rm -rf include/kernel-5.15
+#cp -r immortalwrt/include/kernel-5.15 include/
+#rm -rf package/boot/{arm-trusted-firmware-rockchip,uboot-rockchip}
+#cp -r immortalwrt/package/boot/{arm-trusted-firmware-rockchip,uboot-rockchip} package/boot/
+#cp -r target/linux/generic/pending-5.15/{900-driver2305.patch,900-option.patch,900-qcserial.patch} immortalwrt/target/linux/generic/pending-5.15
+#rm -rf target/linux/generic
+#cp -r immortalwrt/target/linux/generic target/linux/
+#rm -rf immortalwrt
 sed -i 's|+wireless-tools ||g' package/rooter/ext-rooter-basic/Makefile
 
 # Add a feed source
