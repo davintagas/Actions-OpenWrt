eb8d63240087f7a18ab4a891da4ef40396fa8997
davintagas <78451092+davintagas@users.noreply.github.com>
2024-06-21 08:21:17 +0700

    Update diy-part2.sh

--- config/ROOter/diy-part2.sh
+++ config/ROOter/diy-part2.sh
@@ -39,8 +39,6 @@ pushd files/etc/uci-defaults
 wget https://raw.githubusercontent.com/davintagas/default/main/official/99-init-settings.sh
 popd
 
-sed -i 's|+ext-wireguard +build-usb|+ext-wireguard|g' package/rooter-custom-builds/ext-custom-level3/Makefile
-
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
