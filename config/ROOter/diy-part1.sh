f2aaac3e29c2fcb0cc30048f205d0407955b395b
davintagas <78451092+davintagas@users.noreply.github.com>
2024-06-22 20:20:03 +0700

    Update diy-part1.sh

--- config/ROOter/diy-part1.sh
+++ config/ROOter/diy-part1.sh
@@ -10,36 +10,17 @@
 # See /LICENSE for more information.
 #
 
-# Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-# echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-#pushd target/linux/generic/backport-5.15/
-#wget https://raw.githubusercontent.com/openwrt/openwrt/80ef582deebd13e3a46718f4012947e4b56f31cf/target/linux/generic/backport-5.15/791-v6.6-11-net-phy-motorcomm-Add-pad-drive-strength-cfg-support.patch
-#popd
-
-git clone -b v23.05.3 --depth 1 https://github.com/openwrt/openwrt.git lede
-cp -r target/linux/generic/pending-5.15/{900-driver2305.patch,900-option.patch,900-qcserial.patch} lede/target/linux/generic/pending-5.15
-rm -rf target/linux/generic
-cp -r lede/target/linux/generic target/linux/
-rm -rf target/linux/rockchip
-cp -r lede/target/linux/rockchip target/linux/
-rm -rf package/boot/{uboot-rockchip,arm-trusted-firmware-rockchip}
-cp -r lede/package/boot/{arm-trusted-firmware-rockchip,uboot-rockchip} package/boot/
-cp -rf lede/include/{kernel-5.15,image.mk,kernel-build.mk,kernel-defaults.mk,image-commands.mk,version.mk} include/
-rm -rf target/linux/Makefile
-cp -r lede/target/linux/Makefile target/linux/
-rm -rf target/Makefile package/Makefile
-cp -r lede/target/Makefile target/
-cp -r lede/package/Makefile package/
-cp -rf lede/config/{Config-build.in,Config-kernel.in} config/
-rm -rf lede
+pushd target/linux/generic/backport-5.15/
+wget https://raw.githubusercontent.com/openwrt/openwrt/80ef582deebd13e3a46718f4012947e4b56f31cf/target/linux/generic/backport-5.15/791-v6.6-11-net-phy-motorcomm-Add-pad-drive-strength-cfg-support.patch
+popd
 
 pushd target/linux/rockchip/patches-5.15/
 wget https://raw.githubusercontent.com/immortalwrt/immortalwrt/openwrt-23.05/target/linux/rockchip/patches-5.15/991-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch
-#wget https://raw.githubusercontent.com/openwrt/openwrt/80ef582deebd13e3a46718f4012947e4b56f31cf/target/linux/rockchip/patches-5.15/009-v6.8-arm64-dts-rockchip-configure-eth-pad-driver-strength-for-.patch
+wget https://raw.githubusercontent.com/openwrt/openwrt/80ef582deebd13e3a46718f4012947e4b56f31cf/target/linux/rockchip/patches-5.15/009-v6.8-arm64-dts-rockchip-configure-eth-pad-driver-strength-for-.patch
 popd
+
+rm -rf target/linux/rockchip/image/mmc.bootscript
+wget -O target/linux/rockchip/image/mmc.bootscript https://raw.githubusercontent.com/openwrt/openwrt/openwrt-23.05/target/linux/rockchip/image/mmc.bootscript
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
