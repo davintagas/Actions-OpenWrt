5853090a42e8bcee65634442b06f49907cd7c1b5
davintagas <78451092+davintagas@users.noreply.github.com>
2024-06-26 14:30:48 +0700

    Update diy-part1.sh

--- config/ROOter/diy-part1.sh
+++ config/ROOter/diy-part1.sh
@@ -9,16 +9,16 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-rm -rf target/linux/rockchip/patches-5.4/001-add-rk3328-dmc-driver.patch
+rm -rf tmp
 
 #pushd target/linux/generic/backport-5.15/
 #wget https://raw.githubusercontent.com/openwrt/openwrt/80ef582deebd13e3a46718f4012947e4b56f31cf/target/linux/generic/backport-5.15/791-v6.6-11-net-phy-motorcomm-Add-pad-drive-strength-cfg-support.patch
 #popd
 
-#pushd target/linux/rockchip/patches-5.15/
-#wget https://raw.githubusercontent.com/immortalwrt/immortalwrt/openwrt-23.05/target/linux/rockchip/patches-5.15/991-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch
+pushd target/linux/rockchip/patches-5.15/
+wget https://raw.githubusercontent.com/immortalwrt/immortalwrt/openwrt-23.05/target/linux/rockchip/patches-5.15/991-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch
 #wget https://raw.githubusercontent.com/openwrt/openwrt/80ef582deebd13e3a46718f4012947e4b56f31cf/target/linux/rockchip/patches-5.15/009-v6.8-arm64-dts-rockchip-configure-eth-pad-driver-strength-for-.patch
-#popd
+popd
 
 #rm -rf target/linux/rockchip/image/mmc.bootscript
 #wget -O target/linux/rockchip/image/mmc.bootscript https://raw.githubusercontent.com/openwrt/openwrt/openwrt-23.05/target/linux/rockchip/image/mmc.bootscript
