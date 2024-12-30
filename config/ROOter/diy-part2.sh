197a932526bfefc5e2811f3553b5106e127e1ec1
davintagas <78451092+davintagas@users.noreply.github.com>
2024-12-30 14:35:43 +0700

    Update diy-part2.sh

--- config/ROOter/diy-part2.sh
+++ config/ROOter/diy-part2.sh
@@ -21,31 +21,10 @@ git clone --depth 1 https://github.com/gSpotx2f/luci-app-internet-detector.git i
 mv internet-detector/{luci-app-internet-detector,internet-detector,internet-detector-mod-modem-restart} package/
 rm -rf internet-detector
 
-# Add theme
-#git clone -b js --depth 1 https://github.com/gngpp/luci-theme-design.git package/luci-theme-design
-#git clone --depth 1 https://github.com/derisamedia/luci-theme-alpha.git package/luci-theme-alpha
-
-# Add mortal
-#git clone -b openwrt-23.05 --depth 1 https://github.com/immortalwrt/luci.git mortal
-#rm -rf feeds/luci/modules/{luci-base,luci-mod-status}
-#cp -r mortal/modules/{luci-base,luci-mod-status} feeds/luci/modules/
-#rm -rf mortal
-
 # Add app
 git clone --depth 1 https://github.com/kiddin9/kwrt-packages.git kiddin
-# Add ramfree
-cp -r kiddin/luci-app-ramfree package/luci-app-ramfree
 # Add cpufreq
 cp -r kiddin/{luci-app-cpufreq,cpufreq} package/
-# Change
-#cp -r kiddin/autocore package/
-#sed -i 's|bcm27xx-utils|bcm27xx-userland|g' package/autocore/Makefile
-# Atinout
-#cp -r kiddin/atinout package/
-# Add Dnsproxy
-#cp -r kiddin/luci-app-dnsproxy package/
-# Add thame
-#cp -r kiddin/luci-theme-kucat package/
 # Adguardhome
 cp -r kiddin/luci-app-adguardhome package/
 rm -rf feeds/packages/net/adguardhome
@@ -58,22 +37,6 @@ rm -rf feeds/packages/net/{v2ray-core,v2ray-geodata}
 git clone --depth 1 -b 4.78-4 https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 git clone --depth 1 -b main https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
 
-# Add openclash
-#git clone --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-#mkdir -p package/luci-app-openclash/luci-app-openclash/root/etc/openclash/core
-#pushd package/luci-app-openclash/luci-app-openclash/root/etc/openclash/core/
-#wget https://github.com/vernesong/OpenClash/raw/refs/heads/core/master/meta/clash-linux-arm64.tar.gz
-#tar -xf clash-linux-arm64.tar.gz
-#rm -rf clash-linux-arm64.tar.gz
-#mv clash clash_meta
-#popd
-
-# Change adblock
-#rm -rf feeds/packages/net/adblock-fast
-#rm -rf feeds/luci/applications/luci-app-adblock-fast
-#git clone --depth 1 https://github.com/stangri/adblock-fast.git feeds/packages/net/adblock-fast
-#git clone --depth 1 https://github.com/stangri/luci-app-adblock-fast.git feeds/luci/applications/luci-app-adblock-fast
-
 # Change Golang
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
@@ -84,23 +47,3 @@ rm -rf feeds/packages/net/{microsocks,sing-box,trojan-go,xray-core}
 # Add partisi change
 git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
 
-# Add Default Setting
-#mkdir -p files/etc/uci-defaults
-#pushd files/etc/uci-defaults
-#wget https://raw.githubusercontent.com/davintagas/default/main/rooter/99-init-settings.sh
-#popd
-
-#pushd files/etc
-#wget https://raw.githubusercontent.com/davintagas/default/main/bash.bashrc
-#popd
-
-#mkdir -p files/etc/profile.d/
-#pushd files/etc/profile.d
-#wget https://raw.githubusercontent.com/davintagas/default/main/30-sysinfo.sh
-#popd
-
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
