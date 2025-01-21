d86635fa02c2930594619b85cda8b69956ef2c6d
davintagas <78451092+davintagas@users.noreply.github.com>
2025-01-21 09:35:46 +0700

    Update diy-part1.sh

--- config/gargoyle/diy-part1.sh
+++ config/gargoyle/diy-part1.sh
@@ -11,3 +11,4 @@
 #
 #sed -i 's|+iptables-nft +ip6tables-nft|+iptables-nft|g' package/rooter/ext-rooter-basic/Makefile
 #sed -i 's|+kmod-rt2800-usb +xxd +kmod-tcp-bbr|+kmod-rt2800-usb|g' package/rooter/ext-rooter-basic/Makefile
+sed -i 's/$num_build_thread_str V=s/$num_build_thread_str || make -j1 || make -j1 V=s/g' build.sh
