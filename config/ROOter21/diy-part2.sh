3560f30398ab72c93cb55400489ed93b6902cc6b
davintagas <78451092+davintagas@users.noreply.github.com>
2025-01-21 14:00:54 +0700

    Update diy-part2.sh

--- config/ROOter21/diy-part2.sh
+++ config/ROOter21/diy-part2.sh
@@ -17,22 +17,22 @@ sed -i 's/root::0:0:99999:7:::/root:$1$wGIpIvlO$H3XgR7517Ex7I6fQ2jHwv0:19786:0:9
 # sed -i 's/root::0:0:99999:7:::/root:$1$wGIpIvlO$H3XgR7517Ex7I6fQ2jHwv0:19786:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Add internet-detector
-#git clone --depth 1 https://github.com/gSpotx2f/luci-app-internet-detector.git internet-detector
-#mv internet-detector/{luci-app-internet-detector,internet-detector,internet-detector-mod-modem-restart} package/
-#rm -rf internet-detector
+git clone --depth 1 https://github.com/gSpotx2f/luci-app-internet-detector.git internet-detector
+mv internet-detector/{luci-app-internet-detector,internet-detector,internet-detector-mod-modem-restart} package/
+rm -rf internet-detector
 
 # Add app
-#git clone --depth 1 https://github.com/kiddin9/kwrt-packages.git kiddin
+git clone --depth 1 https://github.com/kiddin9/kwrt-packages.git kiddin
 # Add cpufreq
-#cp -r kiddin/{luci-app-cpufreq,cpufreq} package/
+cp -r kiddin/{luci-app-cpufreq,cpufreq} package/
 # Atinout
-#cp -r kiddin/{luci-app-atinout,atinout} package/
+cp -r kiddin/{luci-app-atinout,atinout} package/
 # Adguardhome
-#cp -r kiddin/luci-app-adguardhome package/
+cp -r kiddin/luci-app-adguardhome package/
 #rm -rf feeds/packages/net/adguardhome
 #cp -r kiddin/adguardhome feeds/packages/net/
 # Delete
-#rm -rf kiddin
+rm -rf kiddin
 
 # Add passwall
 #rm -rf feeds/packages/net/{v2ray-core,v2ray-geodata}
