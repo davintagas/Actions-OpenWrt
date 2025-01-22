a027d3686add2f8e90078aeb5625cec3f07272f8
davintagas <78451092+davintagas@users.noreply.github.com>
2025-01-22 09:06:23 +0700

    Update diy-part2.sh

--- config/gargoyle/diy-part2.sh
+++ config/gargoyle/diy-part2.sh
@@ -13,3 +13,11 @@ git clone --depth 1 https://github.com/koshev-msk/modemfeed.git modemfeed
 cp -r packages/net/xmm-modem package/
 rm -rf modemfeed
 
+git clone --depth https://github.com/obsy/packages.git obsy_packages
+cp -rf obsy_packages/{3ginfo,modemband,plugin-gargoyle-*} package/
+rm -rf obsy_packages
+
+mkdir -p package/sms-tool
+pushd package/sms-tool
+wget https://raw.githubusercontent.com/openwrt/packages/refs/heads/openwrt-23.05/utils/sms-tool/Makefile
+popd
