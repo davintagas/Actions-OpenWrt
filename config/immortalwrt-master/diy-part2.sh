77a43a30fbe600fdd03dceb1a388432febbda852
davintagas <78451092+davintagas@users.noreply.github.com>
2025-02-04 13:37:19 +0700

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -15,10 +15,6 @@
 # Add the default password for the 'root' user（Change the empty password to 'password'）
 sed -i 's/root:::0:99999:7:::/root:$1$wGIpIvlO$H3XgR7517Ex7I6fQ2jHwv0:19786:0:99999:7:::/g' package/base-files/files/etc/shadow
 
-# Add theme
-# git clone -b js --depth 1 https://github.com/gngpp/luci-theme-design.git package/luci-theme-design
-git clone --depth 1 https://github.com/derisamedia/luci-theme-alpha.git package/luci-theme-alpha
-
 # Add Package From Kiddin
 git clone --depth 1 https://github.com/kiddin9/openwrt-packages.git kiddin
 # Add driver l860-GL
@@ -26,11 +22,14 @@ cp -r kiddin/{luci-proto-xmm,xmm-modem} package/
 # Add Atinout
 cp -r kiddin/{luci-app-atinout,atinout} package/
 # Add internet-detector
-cp -r kiddin/luci-app-internet-detector package/luci-app-internet-detector
-# DNSProxy
-cp -r kiddin/{luci-app-dnsproxy,dnsproxy} package/
+cp -r kiddin/{luci-app-internet-detector,internet-detector} package/
+# adh
+cp -r kiddin/luci-app-adguardhome package/luci-app-adguardhome
 # Delete Source
 rm -rf kiddin
+
+sed -i 's|Shanghai|Jakarta|g' package/emortal/default-settings/files/99-default-settings-chinese
+sed -i 's|https://mirrors.vsean.net/openwrt|https://immortalwrt.kyarucloud.moe|g' package/emortal/default-settings/files/99-default-settings-chinese
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
