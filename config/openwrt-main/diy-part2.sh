c7f153f3a98a2fc169c00f326394c987b5a01837
davintagas <78451092+davintagas@users.noreply.github.com>
2025-02-13 08:12:49 +0700

    Update diy-part2.sh

--- config/openwrt-main/diy-part2.sh
+++ config/openwrt-main/diy-part2.sh
@@ -15,6 +15,16 @@
 # Add the default password for the 'root' user（Change the empty password to 'password'）
 sed -i 's/root:::0:99999:7:::/root:$1$wGIpIvlO$H3XgR7517Ex7I6fQ2jHwv0:19786:0:99999:7:::/g' package/base-files/files/etc/shadow
 
+# Add Custom package
+git clone --depth 1 https://github.com/kiddin9/kwrt-packages.git kiddin
+cp -rf kiddin/luci-app-3ginfo-lite package/luci-app-3ginfo-lite
+cp -rf kiddin/{luci-proto-xmm,xmm-modem} package/
+cp -rf kiddin/{luci-app-atinout,atinout} package/
+cp -rf kiddin/luci-app-sms-tool-js package/luci-app-sms-tool-js
+cp -rf kiddin/{luci-app-internet-detector,internet-detector} package/
+cp -rf kiddin/{luci-app-cpufreq,cpufreq} package/
+cp -rf kiddin/{luci-app-diskman,luci-app-disks-info} package/
+
 # Add Default Setting
 mkdir -p files/etc/uci-defaults
 pushd files/etc/uci-defaults
