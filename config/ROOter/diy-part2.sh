cfa367d7d0d1df84d5ac8418427f9983a2c10768
davintagas <78451092+davintagas@users.noreply.github.com>
2025-03-20 10:43:35 +0700

    Update diy-part2.sh

--- config/ROOter/diy-part2.sh
+++ config/ROOter/diy-part2.sh
@@ -31,4 +31,4 @@ rm -rf kiddin
 
 # Golang
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
