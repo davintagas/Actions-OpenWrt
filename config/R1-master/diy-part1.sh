97c7be5742f643df0871e485a1533404307e0fc2
davintagas <78451092+davintagas@users.noreply.github.com>
2025-04-10 22:17:13 +0700

    Update diy-part1.sh

--- config/R1-master/diy-part1.sh
+++ config/R1-master/diy-part1.sh
@@ -13,6 +13,9 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+rm -rf package/kernel/linux/modules/netsupport.mk
+wget -O package/kernel/linux/modules/netsupport.mk https://raw.githubusercontent.com/davintagas/default/refs/heads/main/netsupport.mk
+ 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
