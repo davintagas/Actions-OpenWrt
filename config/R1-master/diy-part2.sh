20ace31bf9342e594de8201a27706939082b4823
davintagas <78451092+davintagas@users.noreply.github.com>
2025-04-10 08:22:07 +0700

    Update diy-part2.sh

--- config/R1-master/diy-part2.sh
+++ config/R1-master/diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.2.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.2.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 # Add the default password for the 'root' user（Change the empty password to 'password'）
 sed -i 's/root::0:0:99999:7:::/root:$1$wGIpIvlO$H3XgR7517Ex7I6fQ2jHwv0:19786:0:99999:7:::/g' package/base-files/files/etc/shadow
 
@@ -28,11 +28,11 @@ cp -r kiddin/luci-app-adguardhome package/luci-app-adguardhome
 # sms-tool
 cp -r kiddin/{luci-app-sms-tool-js,sms-tool} package/
 # 3ginfo
-cp -r kiddin/{luci-app-3ginfo-lite,autocore} package/
+cp -r kiddin/luci-app-3ginfo-lite package/
+# qosmate
+cp -rf kiddin/{luci-app-qosmate,qosmate} package/
 # Delete Source
 rm -rf kiddin
-
-sed -i 's|bcm27xx-utils|bcm27xx-userland|g' package/autocore/Makefile
 
 # golang
 rm -rf feeds/packages/lang/golang
@@ -43,12 +43,6 @@ git clone --depth 1 -b master https://github.com/immortalwrt/packages.git mortal
 rm -rf feeds/packages/net/adguardhome
 cp -r mortal-packages/net/adguardhome feeds/packages/net/
 rm -rf mortal-packages
-
-# change base
-git clone --depth 1 -b openwrt-21.02 https://github.com/immortalwrt/luci.git mortal-luci
-rm -rf feeds/luci/modules/{luci-base,luci-mod-status}
-cp -rf mortal-luci/modules/{luci-base,luci-mod-status} feeds/luci/modules/
-rm -rf mortal-luci
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
