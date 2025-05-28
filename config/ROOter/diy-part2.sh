8618e5b9356d3e3192686074690019084e30369d
davintagas <78451092+davintagas@users.noreply.github.com>
2025-05-28 10:23:16 +0700

    Update diy-part2.sh

--- config/ROOter/diy-part2.sh
+++ config/ROOter/diy-part2.sh
@@ -34,6 +34,10 @@ git clone --depth 1 -b v0.46.086 https://github.com/vernesong/OpenClash.git pack
 cp -rf package/openclash/luci-app-openclash package/luci-app-openclash
 rm -rf package/openclash
 
+pushd package/luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
+
 # Golang
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
