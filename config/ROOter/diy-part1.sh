d7e8755e95f6ffd62cdfa765bf861a680ce6de83
davintagas <78451092+davintagas@users.noreply.github.com>
2025-05-28 13:24:32 +0700

    Update diy-part1.sh

--- config/ROOter/diy-part1.sh
+++ config/ROOter/diy-part1.sh
@@ -30,6 +30,8 @@
 #wget https://raw.githubusercontent.com/immortalwrt/immortalwrt/refs/heads/master/target/linux/rockchip/patches-6.6/991-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch
 #popd
 
+make dirclean
+
 #git clone --depth 1 https://github.com/davintagas/davin.git configfiles/davin
 
 # Add a feed source
