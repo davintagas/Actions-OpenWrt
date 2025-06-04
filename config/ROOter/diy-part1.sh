294e68d9509ad8a56587a6072cf3503d194182c5
davintagas <78451092+davintagas@users.noreply.github.com>
2025-06-04 13:13:54 +0700

    Update diy-part1.sh

--- config/ROOter/diy-part1.sh
+++ config/ROOter/diy-part1.sh
@@ -11,28 +11,10 @@
 #
 #sed -i 's|+iptables-nft +ip6tables-nft|+iptables-nft|g' package/rooter/ext-rooter-basic/Makefile
 #sed -i 's|+kmod-rt2800-usb +xxd +kmod-tcp-bbr|+kmod-rt2800-usb|g' package/rooter/ext-rooter-basic/Makefile
-# ROOter 23.05
-#wget -O configfiles/template/.config_r1+ https://raw.githubusercontent.com/davintagas/default/refs/heads/main/ROOter/.config_r1%2B
-#rm -rf router2305.json
-#rm -rf build
-#wget https://raw.githubusercontent.com/davintagas/default/refs/heads/main/ROOter/router2305.json
-#wget https://raw.githubusercontent.com/davintagas/default/refs/heads/main/ROOter/build
-#chmod +x build
-# ROOter Main
-#wget -O configfiles/template/.config_r1+lts https://raw.githubusercontent.com/davintagas/default/refs/heads/main/ROOter/build-main/.config_r1%2Blts
-#rm -rf routermaster.json
-#rm -rf build
-#wget https://raw.githubusercontent.com/davintagas/default/refs/heads/main/ROOter/build-main/routermaster.json
-#wget https://raw.githubusercontent.com/davintagas/default/refs/heads/main/ROOter/build-main/build
-#chmod +x build
 
-#pushd target/linux/rockchip/patches-5.15
-#wget https://raw.githubusercontent.com/immortalwrt/immortalwrt/refs/heads/master/target/linux/rockchip/patches-6.6/991-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch
-#popd
-
-#make dirclean
-
-#git clone --depth 1 https://github.com/davintagas/davin.git configfiles/davin
+pushd target/linux/rockchip/patches-5.15
+wget https://raw.githubusercontent.com/immortalwrt/immortalwrt/refs/heads/master/target/linux/rockchip/patches-6.6/991-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch
+popd
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
