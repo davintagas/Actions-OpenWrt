d4235c5db6c29fc77da75a700c8d85a69d64616f
davintagas <78451092+davintagas@users.noreply.github.com>
2025-06-07 06:37:03 +0700

    Update diy-part2.sh

--- config/orangepi-21.02/diy-part2.sh
+++ config/orangepi-21.02/diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-# sed -i 's/192.168.2.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.31.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 # Add the default password for the 'root' user（Change the empty password to 'password'）
 sed -i 's/root::0:0:99999:7:::/root:$1$wGIpIvlO$H3XgR7517Ex7I6fQ2jHwv0:19786:0:99999:7:::/g' package/base-files/files/etc/shadow
 
