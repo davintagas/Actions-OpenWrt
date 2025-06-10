11261a5423949001a610c50a5c6be78f5728dc9b
davintagas <davintagas@gmail.com>
2025-06-10 08:46:47 +0700

    modif lede config

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -37,23 +37,4 @@ rm -rf kiddin
 
 sed -i 's|CST-8|WIB-7|g' package/lean/default-settings/files/zzz-default-settings
 sed -i 's|Shanghai|Jakarta|g' package/lean/default-settings/files/zzz-default-settings
-
-# OpenClash
-rm -rf feeds/luci/applications/luci-app-openclash
-git clone --depth 1 -b v0.46.086 https://github.com/vernesong/OpenClash.git package/openclash
-cp -rf package/openclash/luci-app-openclash package/luci-app-openclash
-rm -rf package/openclash
-pushd package/luci-app-openclash/tools/po2lmo
-make && sudo make install
-popd
-
-mkdir -p files/etc/openclash/core
-pushd files/etc/openclash/core
-wget https://github.com/vernesong/OpenClash/raw/refs/heads/core/master/meta/clash-linux-arm64.tar.gz
-tar -xf clash-linux-arm64.tar.gz
-rm -rf clash-linux-arm64.tar.gz
-mv clash clash_meta
-popd
-
-# Modify hostname
-sed -i 's/OpenWrt/OrangePi/g' package/base-files/files/bin/config_generate
+#
