fe358b36d2d8ac2dc11690bcd9a13b1b24a7b9e6
davintagas <78451092+davintagas@users.noreply.github.com>
2025-06-12 13:06:24 +0700

    Update diy-part1.sh

--- config/ROOter-21/diy-part1.sh
+++ config/ROOter-21/diy-part1.sh
@@ -9,6 +9,9 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
+rm -rf target/linux/rockchip/armv8/base-files/etc/init.d/resize-rootfs
+rm -rf target/linux/rockchip/armv8/base-files/etc/rc.d/S22resize-rootfs
+rm -rf target/linux/rockchip/armv8/base-files/usr/bin/resize-rootfs.sh
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
