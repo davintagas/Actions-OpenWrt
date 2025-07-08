8a5e17afe9f74ae876a6f624f9ad9b368ad6cb50
davintagas <78451092+davintagas@users.noreply.github.com>
2025-07-08 15:59:16 +0700

    Update diy-part1.sh

--- lede-master/diy-part1.sh
+++ lede-master/diy-part1.sh
@@ -17,5 +17,9 @@
 
 sed -i 's|CST-8|WIB-7|g' package/lean/default-settings/files/zzz-default-settings
 sed -i 's|Shanghai|Jakarta|g' package/lean/default-settings/files/zzz-default-settings
+
+rm -rf target/linux/rockchip/Makefile
+wget -O target/linux/rockchip/Makefile https://raw.githubusercontent.com/davintagas/default/refs/heads/main/lede/Makefile
+
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
