7cd6a254827bab192779ad68b38a29e8dd18152e
davintagas <davintagas@gmail.com>
2025-07-14 08:44:46 +0700

    modif

--- lede-master/diy-part2.sh
+++ lede-master/diy-part2.sh
@@ -15,7 +15,29 @@
 # Add the default password for the 'root' user（Change the empty password to 'password'）
 sed -i 's/root:::0:99999:7:::/root:$1$wGIpIvlO$H3XgR7517Ex7I6fQ2jHwv0:19786:0:99999:7:::/g' package/base-files/files/etc/shadow
 
-# openclash
+# Add Custom Package
+git clone --depth 1 https://github.com/kiddin9/kwrt-packages.git kiddin
+
+# Add 3ginfo
+cp -rf kiddin/luci-app-3ginfo-lite package/
+
+# Add internet-detector
+cp -rf kiddin/{luci-app-internet-detector,internet-detector} package/
+
+# Add AT
+cp -rf kiddin/{luci-app-at-socat,atinout} package/
+pushd luci-app-at-socat/root/etc/modem
+rm -rf *
+wget -O atsocatcommands.user https://raw.githubusercontent.com/davintagas/default/refs/heads/main/atcommands.user
+popd
+
+# Add xmm
+cp -rf kiddin/{luci-proto-xmm,xmm-modem} package/
+
+# Remove
+rm -rf kiddin
+
+# OpenClash
 rm -rf feeds/luci/applications/luci-app-openclash
 git clone --depth 1 -b v0.46.120 https://github.com/vernesong/OpenClash.git package/OpenClash
 mv package/OpenClash/luci-app-openclash package/
@@ -32,33 +54,9 @@ rm -rf clash-linux-arm64.tar.gz
 mv clash clash_meta
 popd
 
-# atc
-#git clone --depth 1 https://github.com/mrhaav/openwrt-packages.git package/atc
-#mv package/atc/{luci,atc-fib-l850_gl} package/
-#rm -rf package/atc
-#rm -rf package/atc-fib-l850_gl/files/lib/netifd/proto/atc.sh
-#pushd package/atc-fib-l850_gl/files/lib/netifd/proto
-#wget https://raw.githubusercontent.com/davintagas/default/refs/heads/main/atc.sh
-#popd
-
-# 3ginfo
-git clone --depth 1 https://github.com/4IceG/luci-app-3ginfo-lite.git package/luci-app-3ginfo-lite
-rm -rf package/luci-app-3ginfo-lite/sms-tool
-
-# detector
-git clone --depth 1 https://github.com/gSpotx2f/luci-app-internet-detector.git package/luci-app-internet-detector
-
-# modemfeed
-git clone --depth 1 https://github.com/koshev-msk/modemfeed.git package/modemfeed
-mv package/modemfeed/luci/protocols/luci-proto-xmm package/
-mv package/modemfeed/packages/net/xmm-modem package/
-rm -rf package/modemfeed
-
-# atinout
-git clone --depth 1 https://github.com/4IceG/luci-app-atinout.git package/luci-app-atinout
-
 # sms-tool
 git clone --depth 1 https://github.com/4IceG/luci-app-sms-tool.git package/luci-app-sms-tool
 
+rm -rf feeds/luci/applications/luci-app-passwall
 #
 
